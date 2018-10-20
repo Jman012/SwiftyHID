a8000a4daaf265fb41b9038309399c707542a809
Jman012 <jman012guy@gmail.com>
2018-10-20 12:31:35 -0700

    Initial Commit

---
+++ SwiftyHID/SwiftyHID.h
@@ -0,0 +1,13 @@
+//
+//  SwiftyHID.h
+//  SwiftyHID
+//
+//  Created by James Linnell on 10/20/18.
+//  Copyright © 2018 James Linnell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwiftyHID : NSObject
+
+@end
